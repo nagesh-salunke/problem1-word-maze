bf370df1858a14e764c3dc49ddbaacc73349413c
Nagesh Salunke <salunkenagesh14@gmail.com>
2025-09-27 02:32:27 +0530

    Add problem 1 word maze

---
+++ README.md
@@ -0,0 +1,35 @@
+# Word Maze
+
+Given a 2D grid of characters and a word, determine if the word exists in the grid by moving to adjacent cells (up, down, left, right). Each cell can be used at most once in the path.
+
+## Input
+N M
+grid of N rows, M columns
+word
+
+## Constraints
+1 ≤ N, M ≤ 10
+Word length ≤ 20
+
+## Output
+YES or NO
+
+## Examples
+Input:
+3 4
+ABCE
+SFCS
+ADEE
+ABCCED
+Output:
+YES
+
+Input:
+3 4
+ABCE
+SFCS
+ADEE
+SEE
+Output:
+YES
+
---
+++ Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        sc.nextLine();
+        char[][] grid = new char[N][M];
+        for(int i=0;i<N;i++){
+            String line = sc.nextLine();
+            for(int j=0;j<M;j++){
+                grid[i][j] = line.charAt(j);
+            }
+        }
+        String word = sc.nextLine();
+        // TODO: implement solution
+        System.out.println("YES");
+    }
+}
+
